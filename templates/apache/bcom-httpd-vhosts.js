692973d6feef15e764010d7a951b574e60cebc60
Aori Nevo <aori.nevo@bloomingdales.com>
2017-02-28 13:37:38 -0500

    WDSDO-24483: Fix template.

--- templates/apache/bcom-httpd-vhosts.js
+++ templates/apache/bcom-httpd-vhosts.js
@@ -79,7 +79,7 @@ Listen 443
     ProxyPass /loyallist/benefits http://${data.domainPrefix}.bloomingdales.fds.com:9080/loyallist/benefits
     ProxyPass /loyallist https://${data.domainPrefix}.bloomingdales.fds.com:9443/loyallist
     ProxyPass /chkout https://${data.domainPrefix}.bloomingdales.fds.com:9443/chkout
-    ProxyPass /wedding-registry https://${data.domainPrefix}.bloomingdales.fds.com/wedding-registry/
+    ProxyPass /wedding-registry https://${data.domainPrefix}.${data.envName}.fds.com/wedding-registry/
     ProxyPass /registry https://${data.domainPrefix}.bloomingdales.fds.com:9443/registry
 
     # NavApp Assets
